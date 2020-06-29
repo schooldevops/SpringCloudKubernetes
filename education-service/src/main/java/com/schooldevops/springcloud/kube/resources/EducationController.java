53750afd4e3ee69bfdde45b1c4e1a2666186ed38
schooldevops <schooldevops@gmail.com>
2020-06-29 18:01:15 +0900

    use config map

--- education-service/src/main/java/com/schooldevops/springcloud/kube/resources/EducationController.java
+++ education-service/src/main/java/com/schooldevops/springcloud/kube/resources/EducationController.java
@@ -1,10 +1,13 @@
 package com.schooldevops.springcloud.kube.resources;
 
+import com.google.api.client.util.Value;
+import com.schooldevops.springcloud.kube.configs.PropertiesConfig;
 import com.schooldevops.springcloud.kube.models.Education;
 import com.schooldevops.springcloud.kube.models.Student;
 import com.schooldevops.springcloud.kube.services.EducationService;
 import com.schooldevops.springcloud.kube.services.StudentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +20,20 @@ import java.util.List;
 @RequestMapping("/api/educations")
 public class EducationController {
 
+    @Autowired
+    private PropertiesConfig propertiesConfig;
+
     private final EducationService educationService;
     private final StudentService studentService;
 
     public EducationController(EducationService educationService, StudentService studentService) {
         this.educationService = educationService;
         this.studentService = studentService;
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return propertiesConfig.getMessage();
     }
 
     @GetMapping("/servers")
